9c9df200fd9b1ae56a19c137e35c7d743de56825
jimestir <jimmyestir@gmail.com>
2023-02-14 14:45:44 +0100

    only operations with keys are missing

--- app.js
+++ app.js
@@ -13,52 +13,62 @@ class Stack {
   }
 }
 
-function detectedParent(operation) {
-  let s = new Stack();
-  let lastPosition = null;
-  let boolean = null;
-  let BRACKETS = {
-    ")": () => {
-      if (s.dataStore[s.length() - 1][0] === "(") return s.pop();
-      else if (s.dataStore[s.length() - 1] === undefined) return lastPosition;
-    },
-
-    "]": () => {
-      if (s.dataStore[s.length() - 1][0] === "[") return s.pop();
-      else if (s.dataStore[s.length() - 1] === undefined) return lastPosition;
-    },
-
-    "}": () => {
-      if (s.dataStore[s.length() - 1][0] === "{") return s.pop();
-      else if (s.dataStore[s.length() - 1][0] === undefined)
-        return lastPosition;
-    },
+function eval(numOne, numTwo, op) {
+  let OPERATIONS = {
+    "*": parseInt(numOne) * parseInt(numTwo),
+    "+": parseInt(numOne) + parseInt(numTwo),
+    "-": parseInt(numOne) - parseInt(numTwo),
+    "/": parseInt(numOne) / parseInt(numTwo),
   };
+  return OPERATIONS[op];
+}
 
-  for (let i = 0; i < operation.length; i++) {
-    if ("(" === operation[i] || "[" === operation[i] || "{" === operation[i]) {
-      s.push([operation[i], i]);
-      lastPosition = i;
-    } else if (
-      ")" === operation[i] ||
-      "]" === operation[i] ||
-      "}" === operation[i]
-    ) {
-      if (s.dataStore[0] === undefined)
-        return `The bracket of aperture is in position ${lastPosition}`;
-      boolean = BRACKETS[operation[i]]();
-      if (!boolean) return i;
+function calculate(operation) {
+  let OPERATORS = {
+    "*": true,
+    "/": true,
+    "+": true,
+    "-": true,
+  };
+  let i = 0;
+  let application = 0;
+  do {
+    if (OPERATORS[operation[i]]) {
+      let application = eval(operation[i - 1], operation[i - 2], operation[i]);
+      operation.splice(i - 2, 3, application);
+      console.log(operation.length, { i });
+      console.log(operation);
+      i = operation.length > 3 ? 3 : 2;
+    } else i++;
+  } while (operation.length > 3);
+  application = eval(operation[i - 1], operation[i - 2], operation[i]);
+  operation.splice(i - 2, 3, application);
+
+  return operation;
+}
+function infixesToPostfixed(operation) {
+  let arrNumOpe = operation.split(/[+(\)\-*]/).filter((e) => e !== "");
+  let arrOperating = operation.replace(/[0-9]/g, "").split("");
+  let operator = new Stack();
+  let result = [];
+  let index = 0;
+  let regroup = null;
+  for (let i = 0; i < arrOperating.length; i++) {
+    if (arrOperating[i] === ")") {
+      index = operator.dataStore.indexOf("(");
+      regroup = operator.dataStore.splice(index + 1, operator.length());
+      operator.dataStore.splice(index, 1);
+      result.push(...regroup);
+      result.push(arrNumOpe[i]);
+    } else {
+      operator.push(arrOperating[i]);
+      result.push(arrNumOpe[i]);
     }
   }
-  return boolean ? true : operation.length;
+
+  result.push(...operator.dataStore.reverse());
+  return calculate(result.filter((e) => e !== undefined));
+  // return result.filter((e) => e !== undefined);
 }
-console.log(detectedParent("2.3+23/12+{[(3.14159*.24)]}")); //true
-console.log(detectedParent("2.3+23/12+(3.14159*.24")); //false
-console.log(detectedParent("((hello))")); //true
-console.log(detectedParent("((=102))))")); //false
-console.log(detectedParent("(((({(({hal))=102))))")); //false
-console.log(
-  detectedParent(
-    "(((({(({hafadfa)))}}}{{{}}}}}{[][][]]]]]]]](((((((((((al))=102))))"
-  )
-); //false
+a = ["10+(1+2)*2", "10+(1+2-1)*2", "[1*(2-3)]+(4+5)"];
+console.log(infixesToPostfixed(a[1]));
