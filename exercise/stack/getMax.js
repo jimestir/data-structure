3ac14b4223213f6104ba41f9f88e65229ea24596
jimestir <jimmyestir@gmail.com>
2023-03-07 11:16:33 +0100

    fit the to ejecution time

--- exercise/stack/getMax.js
+++ exercise/stack/getMax.js
@@ -1,19 +1,47 @@
 const { Stack } = require("../../lib/Stack");
 
 function getMax(operations) {
-  const OPE = operations.map((e) => e.split(" "));
+  const answer = operations.map((e) => e.split(" "));
   const stackOpe = new Stack();
-  const MAX = new Stack();
+  const stackMax = new Stack();
+  const result = [];
+  let poppedValue = 0;
+  let maxValue = 0;
+
   const CONDITIONS = {
-    1: (e) => stackOpe.push(e),
-    2: () => stackOpe.pop(),
-    3: () => MAX.push(Math.max(...stackOpe.dataStore)),
+    1: (value) => {
+      stackOpe.push(value);
+      if (maxValue <= value) {
+        maxValue = value;
+        stackMax.push(maxValue);
+      }
+    },
+    2: () => {
+      poppedValue = stackOpe.pop();
+      if (poppedValue === maxValue) {
+        stackMax.pop();
+        maxValue = stackMax.peek() || 0;
+      }
+    },
+    3: () => result.push(maxValue),
   };
 
-  OPE.forEach((e) => CONDITIONS[e[0]](e[1]));
+  answer.forEach((e) => CONDITIONS[e[0]](Number(e[1])));
 
-  return MAX.dataStore;
+  return result;
 }
-console.log(
-  getMax(["1 97", "2", "1 20", "2", "1 26", "1 20", "2", "3", "1 91", "3"])
-);
+
+let test = [
+  "1 72157563",
+  "2",
+  "1 20",
+  "2",
+  "1 26",
+  "1 28",
+  "2",
+  "3",
+  "1 91",
+  "3",
+];
+
+console.log(getMax(test));
